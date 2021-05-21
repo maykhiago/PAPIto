05e57fc4c8ecbb60b1d0428646255388ec388989
HiagoRocha <hiago.rocha@inf.ufrgs.br>
2021-05-21 12:25:21 -0300

    Installation ok

--- papito_install.sh
+++ papito_install.sh
@@ -3,8 +3,8 @@
 #Obs: Run the following command to make this script executable
 #     chmod +x papito_install.sh
 
-  echo ">>>>>>> Getting the path to the current directory"
-  MY_PATH=$(pwd)
+echo ">>>>>>> Getting the path to the current directory"
+MY_PATH=$(pwd)
 
 cd ..
 
@@ -21,14 +21,14 @@ echo ">>>>>>> Installing"
 make && make install
 
 echo ">>>>>>> Checking if everything is OK"
-cd papi/src/ctests
+cd ctests
 ./serial_hl
 
 echo ">>>>>>> Going to PAPIto directory"
 cd ../../../PAPIto
 
 echo ">>>>>>> Compiling example code"
-gcc simple_array_sum.c -I$PWD/../papi/src/install/include -L$PWD/../papi/src/install/lib $PWD/../papi/src/install/lib/libpapi.a -o simple_array_sum -lpapi
+gcc simple_array_sum.c papito.c -I$PWD/../papi/install/include -L$PWD/../papi/install/lib $PWD/../papi/install/lib/libpapi.a -o simple_array_sum -fopenmp -lpapi
 
 echo ">>>>>>> Running example code"
 ./simple_array_sum
